acd7293e2206ef652f83315e33248effa963d996
Remael99 <remael99@gmail.com>
2023-01-13 15:55:24 +0300

    confetti and shuffle increase

---
+++ hooks/window.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+type WindowSize = {
+  width: number | undefined;
+  height: number | undefined;
+};
+export default function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      console.log(window.innerWidth);
+      setWindowSize({
+        width: window.innerWidth - 50,
+        height: window.innerHeight - 50,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
